0c2548903802883b1b8b5af2a3faa82a3d8cb68f
dspeidel <dspeidel@allegion.com>
2025-04-28 14:59:11 +0200

    test if i am connected to repository

---
+++ teambuilding.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiKiTuTo
+{
+    internal class teamBuilding
+    {
+
+        private List<string> teamList = new List<string>() { "Nikita", "Chris", "Mahdi", "Robin" };
+
+        public void SayHello()
+        {
+            foreach (var teamMember in teamList) 
+            {
+                Console.WriteLine($"Hello {teamMember}");
+            }
+        }
+
+    }
+}
