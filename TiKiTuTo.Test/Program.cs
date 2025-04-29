cf69c1f759f2585c08d37d3974a9d37429039cfe
Nikita Popkov <npopkov@allegion.com>
2025-04-29 17:14:03 +0200

    Directory structure changed because of adding testproject.

---
+++ TiKiTuTo.Test/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.IO;
+using Xunit;
+using TiKiTuTo;
+
+
+public class ProgramTests
+{
+    [Fact]
+    public void TestExitOption()
+    {
+        // Arrange
+        var input = new StringReader("5");
+        var output = new StringWriter();
+
+        Console.SetIn(input);
+        Console.SetOut(output);
+
+        // Act
+        try
+        {
+            Program.Main();
+        }
+        catch (Exception ex)
+        {
+            // Catch the Environment.Exit call
+            Assert.IsType<System.Threading.ThreadAbortException>(ex);
+        }
+
+        // Assert
+        var consoleOutput = output.ToString();
+        Assert.Contains("5. Exit", consoleOutput);
+    }
+}
