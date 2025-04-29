a52bced3d620d9c7b1461507ad1186449357162e
dspeidel <dspeidel@allegion.com>
2025-04-29 09:53:14 +0200

    SayHelloClass added

---
+++ sayHello.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiKiTuTo
+{
+    internal class sayHello
+    {
+
+        public void SayHello(string message) 
+        {
+            Console.WriteLine("Hallo");
+        }
+
+
+    }
+}
