87238916e66b340ea5bd9f148f9e1dbb7d8ad7e0
Mahdi Al-Chab <malchab@allegion.com>
2025-06-11 17:36:38 +0200

    start to create

--- TiKiTuTo/View/ConsoleView.cs
+++ TiKiTuTo/View/ConsoleView.cs
@@ -73,7 +73,7 @@ namespace TiKiTuTo.View
 
             List<string> options = new()
             {
-            "Start new tournament",
+            "Create new tournament",
             "Start tournament based on existing tournament settings",
             "Import tournament settings from import file",
             "Back to Main Menu",
